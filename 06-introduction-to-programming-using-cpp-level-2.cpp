97c48148246aaccd5c2cf56a0eed6142a1231bf8
Faten Eid <fatenfarid1221@gmail.com>
2024-10-26 23:59:32 +0300

    L40

--- 06-introduction-to-programming-using-cpp-level-2.cpp
+++ 06-introduction-to-programming-using-cpp-level-2.cpp
@@ -27,6 +27,7 @@
 #include "L36_DereferencingPointer.h"
 #include "L37_CommonMistakesWithPointers.h"
 #include "L39_CallByReferenceAndUsingPointers.h"
+#include "L40_PointersAndArrays.h"
 
 
 using namespace std;
@@ -34,7 +35,7 @@ using namespace std;
 int main()
 {
 
-	L39_CallByReferenceAndUsingPointers::Task();
+	L40_PointersAndArrays::Task();
 
 
 }
---
+++ L40_PointersAndArrays.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+namespace L40_PointersAndArrays {
+
+	void Task() {
+
+		int arr[4] = { 10,20,30,40 };
+		int* ptr;
+		ptr = arr;
+		//ptr is equivalent to &arr[0];
+		//ptr + 1 is equivalent to &arr[1];
+		//ptr + 2 is equivalent to &arr[2];
+		//ptr + 3 is equivalent to &arr[3];
+		cout << "Addresses are:\n";
+		cout << ptr << endl;
+		cout << ptr + 1 << endl;
+		cout << ptr + 2 << endl;
+		cout << ptr + 3 << endl;
+		cout << "\nValues are: \n";
+		cout << *(ptr) << endl;
+		cout << *(ptr + 1) << endl;
+		cout << *(ptr + 2) << endl;
+		cout << *(ptr + 3) << endl;
+	}
+
+}
